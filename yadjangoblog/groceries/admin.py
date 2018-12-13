5ff2324fcdf27bd203f74ff37f17e3d59772852e
houcan <554105294@qq.com>
2018-12-13 20:08:48 +0800

    no message

--- yadjangoblog/groceries/admin.py
+++ yadjangoblog/groceries/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from groceries.models import CompanyCategory, Company
 
 # Register your models here.
+@admin.register(CompanyCategory)
+class CompanyCategoryAdmin(admin.ModelAdmin):
+    pass
+    
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
