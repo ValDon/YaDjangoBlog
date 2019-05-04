4e6b6453c73470b8f05d062a460962ce118954c3
houcan <554105294@qq.com>
2019-05-04 23:06:35 +0800

    修改 model

--- yadjangoblog/groceries/models.py
+++ yadjangoblog/groceries/models.py
@@ -5,6 +5,8 @@ from django.contrib.gis.db import models
 
 class CompanyCategory(models.Model):
     name = models.CharField(verbose_name="分类", db_index=True, unique=True, null=False, blank=False, max_length=100)
+    icon = models.CharField(verbose_name="图标", db_index=True, unique=False, null=True, blank=False, max_length=50)
+    style = models.CharField(verbose_name="显示风格", db_index=True, unique=False, null=True, blank=False, max_length=50)
     order_num = models.IntegerField(verbose_name="排序", default=0, blank=True, null=True)
 
     def __str__(self):
@@ -17,8 +19,10 @@ class CompanyCategory(models.Model):
 
 class Company(models.Model):
     name = models.CharField(verbose_name="公司名", db_index=True, unique=True, null=False, blank=False, max_length=100)
+    url = models.CharField(verbose_name="链接", null=True, max_length=100)
     logo = models.ImageField(upload_to="logos", default=None)
     desc = models.CharField(verbose_name="描述", max_length=100)
+    hidden = models.BooleanField(verbose_name="是否隐藏", default=False)
     category = models.ForeignKey(CompanyCategory, on_delete=models.CASCADE)
 
     def __str__(self):
